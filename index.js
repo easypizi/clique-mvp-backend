9db305e8dd7a6aace4a9b20cd7a52c4d0746a7d2
Evan Tolstof <itolstof@gmail.com>
2023-03-24 11:16:04 +0700

    implement cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import router from './router.js';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const DB_URL = process.env.MONGO_DB_URL;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', router);
 
 async function startApp() {
