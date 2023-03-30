4f6982287de9f8801fb6b2d7969418a292a38479
Evan Tolstof <itolstof@gmail.com>
2023-03-30 19:26:05 +0700

    remove link as required parameter

--- models/Group.js
+++ models/Group.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // this model is handle data for chat lists
 const Group = new mongoose.Schema({
   group_id: { type: String, required: true },
-  group_link: { type: String, required: true },
+  group_link: { type: String },
   //Show groups for community - get space owner in this array, if yes - show in community space.
   group_admins_id: { type: [String], required: true },
 });
