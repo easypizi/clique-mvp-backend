9b5b2238dd8425e6c80978931eaf43b961804903
Evan Tolstof <itolstof@gmail.com>
2023-04-04 13:29:36 +0700

    update group model with type

--- helpers/groupDataHelpers.js
+++ helpers/groupDataHelpers.js
@@ -3,11 +3,12 @@ export const formatGroupsData = (groups) => {
     return [];
   }
 
-  return groups.map(({ group_name, group_id, group_link }) => {
+  return groups.map(({ group_name, group_id, group_link, group_type }) => {
     return {
       groupId: group_id,
       groupName: group_name,
       groupLink: group_link,
+      groupType: group_type
     };
   });
 };
