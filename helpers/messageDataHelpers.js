c4e46e48d1d5444facb4586061ea8cb7327122f9
Evan Tolstof <itolstof@gmail.com>
2023-04-11 22:51:30 +0700

    fix messages formating on the client

--- helpers/messageDataHelpers.js
+++ helpers/messageDataHelpers.js
@@ -1,21 +1,29 @@
-export const formatMessagesData = ({
-  message_id,
-  message_group_id,
-  message_date,
-  message_text,
-  message_tags,
-  message_link,
-  message_user_photo,
-  message_user_name,
-}) => {
-  return {
-    date: message_date,
-    userName: message_user_name,
-    id: message_id,
-    group_id: message_group_id,
-    tags: message_tags,
-    text: message_text,
-    link: message_link,
-    userPhoto: message_user_photo,
-  };
+export const formatMessagesData = (messages) => {
+  if (!messages || !messages.length) {
+    return [];
+  }
+
+  return messages.map(
+    ({
+      message_id,
+      message_group_id,
+      message_date,
+      message_text,
+      message_tags,
+      message_link,
+      message_user_photo,
+      message_user_name,
+    }) => {
+      return {
+        date: message_date,
+        userName: message_user_name,
+        id: message_id,
+        group_id: message_group_id,
+        tags: message_tags,
+        text: message_text,
+        link: message_link,
+        userPhoto: message_user_photo,
+      };
+    }
+  );
 };
