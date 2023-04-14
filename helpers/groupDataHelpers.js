20f42bf747ce71b6d30d6e3b619d49012f0b06a8
Evan Tolstof <itolstof@gmail.com>
2023-04-14 14:21:09 +0700

    add hidden spaces to groupSpace id

--- helpers/groupDataHelpers.js
+++ helpers/groupDataHelpers.js
@@ -3,12 +3,15 @@ export const formatGroupsData = (groups) => {
     return [];
   }
 
-  return groups.map(({ group_name, group_id, group_link, group_type }) => {
-    return {
-      groupId: group_id,
-      groupName: group_name,
-      groupLink: group_link,
-      groupType: group_type
-    };
-  });
+  return groups.map(
+    ({ group_name, group_id, group_link, group_type, group_hidden_spaces }) => {
+      return {
+        groupId: group_id,
+        groupName: group_name,
+        groupLink: group_link,
+        groupType: group_type,
+        hiddenSpaces: group_hidden_spaces,
+      };
+    }
+  );
 };
